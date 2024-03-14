02131ac8c81c16d0e07ed060e463054645c0504f
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-14 20:53:33 +0600

    application json added

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -6,7 +6,13 @@ import Course from './Course';
 const Courses = ({priceCalc}) => {
     const [courses, setCourses] = useState([]);
     useEffect(() => {
-        fetch('/public/courses.json')
+        fetch(`/public/courses.json`, {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
+      
+          })
             .then(response => response.json())
             .then(data => setCourses(data))
     }, [])
